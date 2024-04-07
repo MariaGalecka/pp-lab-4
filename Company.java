509be97f0276355c7e2b70149bfd386014b63d3e
MariaGalecka <galecm5715195_aehit@students.vizja.pl>
2024-04-07 08:49:05 +0200

    Zadanie 1

--- Company.java
+++ Company.java
@@ -4,10 +4,10 @@ public class Company {
         Employee[] employees = new Employee[5];
 
         employees[0] = new Employee("Jan Kowalski", 4500.0);
-        employees[1] = new Employee("Anna Nowicka", 5000.0);
-        employees[2] = new Employee("Tomasz Zielinski", 6000.0);
-        employees[3] = new Employee("Katarzyna Malinowska", 7000.0);
-        employees[4] = new Employee("Wojciech Adamski", 8000.0);
+        employees[1] = new Employee("Anna Jakubowska", 5000.0);
+        employees[2] = new Employee("Katarzyna Walendowska", 6000.0);
+        employees[3] = new Employee("Maria Perzyna", 7000.0);
+        employees[4] = new Employee("Jerzy Nosowski", 8000.0);
 
         System.out.println("Dane pracownika o indeksie 3:");
         System.out.println(employees[3].toString());
